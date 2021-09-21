23432a5e03981f03572fdc336d61099270c68e84
tgann11 <morganne1170@gmail.com>
2021-09-21 14:22:13 -0500

    added a login and user element, also added functionality to the logout button

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -1,12 +1,18 @@
-const loggedInUser = {
-	id: 1,
-	name: "Morgan",
-	dateJoined: 1630513631166,
-	email: "morganne1170@gmail.com"
+//first step in logged in user: let loggedInUser have an empty object
+let loggedInUser = {}
+
+
+export const logoutUser = () => {
+	loggedInUser = {}
 }
+
 
 export const getLoggedInUser = () => {
 	return loggedInUser;
+}
+
+export const setLoggedInUser = (userObj) => {
+	loggedInUser = userObj;
 }
 
 export const getUsers = () => {
@@ -20,20 +26,20 @@ export const usePostCollection = () => {
 	return [...postCollection];
 }
 
-export const getPosts = () => {
-	return fetch("http://localhost:8088/posts")
-		.then(response => response.json())
-		.then(parsedResponse => {
-			postCollection = parsedResponse
-			return parsedResponse;
-		})
-}
+// export const getPosts = () => {
+// 	return fetch("http://localhost:8088/posts")
+// 		.then(response => response.json())
+// 		.then(parsedResponse => {
+// 			postCollection = parsedResponse
+// 			return parsedResponse;
+// 		})
+// }
 
 export const getSinglePost = (postId) => {
 	return fetch(`http://localhost:8088/posts/${postId}`)
-	  .then(response => response.json())
-  }
-  
+		.then(response => response.json())
+}
+
 
 export const createPost = postObj => {
 	return fetch("http://localhost:8088/posts", {
@@ -53,11 +59,11 @@ export const updatePost = postObj => {
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify(postObj)
-  
+
 	})
 		.then(response => response.json())
-		
-  }
+
+}
 
 export const deletePost = postId => {
 	return fetch(`http://localhost:8088/posts/${postId}`, {
@@ -65,10 +71,54 @@ export const deletePost = postId => {
 		headers: {
 			"Content-Type": "application/json"
 		}
-  
+
 	})
 		.then(response => response.json())
+}
+
+//This function requests the user information from the database.  
+export const loginUser = (userObj) => {
+	return fetch(`http://localhost:8088/users?name=${userObj.name}&email=${userObj.email}`)
+		.then(response => response.json())
+		.then(parsedUser => {
+			//is there a user?
+			console.log("parsedUser", parsedUser) //data is returned as an array
+			if (parsedUser.length > 0) {
+				setLoggedInUser(parsedUser[0]);
+				return getLoggedInUser();
+			} else {
+				//no user
+				return false;
+			}
+		})
+}
+
+//will post a new user to the users table
+export const registerUser = (userObj) => {
+	return fetch(`http://localhost:8088/users`, {
+	  method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(userObj)
+	})
+	.then(response => response.json())
+	.then(parsedUser => {
+	  setLoggedInUser(parsedUser);
+	  return getLoggedInUser();
+	})
   }
 
-
+  //used to know the author of a post, since we have multiple posts. Uses a json feature to expand on the user
+  export const getPosts = () => {
+	const userId = getLoggedInUser().id
+	return fetch(`http://localhost:8088/posts?_expand=user`)
+	  .then(response => response.json())
+	  .then(parsedResponse => {
+		console.log("data with user", parsedResponse)
+		postCollection = parsedResponse
+		return parsedResponse;
+	  })
+  }
+  
   
